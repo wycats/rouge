cda18b4582e46466b2f0fc643ba2ae6953b4b6de
Yehuda Katz <wycats@gmail.com>
2014-07-01 13:51:30 -0700

    Add ES6 module syntax

--- lib/rouge/lexers/javascript.rb
+++ lib/rouge/lexers/javascript.rb
@@ -63,7 +63,7 @@ module Rouge
           abstract boolean byte char class const debugger double enum
           export extends final float goto implements import int interface
           long native package private protected public short static
-          super synchronized throws transient volatile
+          super synchronized throws transient volatile from as
         )
       end
 
